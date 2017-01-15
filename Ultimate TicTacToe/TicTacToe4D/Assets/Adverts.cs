2def0cd9e7ffb71eb9f3216ad1f8eeb222f66fc2
ykwoo <yankit.woo@gmail.com>
2017-01-15 05:39:57 -0800

    Unity Ads  sstuff

---
+++ Ultimate TicTacToe/TicTacToe4D/Assets/Adverts.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Adverts : MonoBehaviour
+{
+	bool show = false;
+	void Start()
+	{
+		Advertisement.Initialize("1268576",true);
+		Debug.Log(Advertisement.isInitialized);
+		Debug.Log(Advertisement.testMode);
+		Debug.Log(Advertisement.isSupported);
+	}
+	void Update()
+	{
+		if(!show)
+			if(Advertisement.IsReady())
+			{
+				show = true;
+				Debug.Log("showing");
+				Advertisement.Show();
+			}
+		//Debug.Log(Advertisement.IsReady());
+	}
+  public void ShowAd()
+  {
+  	
+    if (Advertisement.IsReady())
+    {
+      //Advertisement.Show();
+    }
+  }
+}
