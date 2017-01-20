3e594291b6898b555bd78f7efe697a4a3e0c8d1f
shikang <shikang.n@gmail.com>
2017-01-20 22:41:03 +0800

    main menu loader

---
+++ Ultimate TicTacToe/TicTacToe4D/Assets/Scripts/MainMenuLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		SaveLoad.Load();
+		AvatarHandler.Instance.SetMyAvatarName(GameData.current.avatarName);
+		AvatarHandler.Instance.SetMyAvatarIcon(GameData.current.avatarIcon);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
