67c41e024f428357c0b424e571f87a8e996f2fc7
SolidRocker <kamtk.tyler@gmail.com>
2017-03-02 00:23:27 +0800

    Added Splash Screen

--- Ultimate TicTacToe/TicTacToe4D/Assets/Scripts/SplashScript.cs
+++ Ultimate TicTacToe/TicTacToe4D/Assets/Scripts/SplashScript.cs
@@ -4,23 +4,15 @@ using System.Collections;
 
 public class SplashScript : MonoBehaviour
 {
-	float splashTime;
 	public GameObject WWP;
 
 	void Start ()
 	{
-		splashTime = 2.8f;
 	}
 
 	void Update ()
 	{
-		Color tempColor = WWP.GetComponent<SpriteRenderer>().color;
-		splashTime -= Time.deltaTime;
-		if(splashTime <= 1.0f)
-			tempColor.a -= 1.5f * Time.deltaTime;
-		if(splashTime <= 0.5f)
+		if(WWP.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("EndAnim"))
 			SceneManager.LoadScene("MainMenu");
-
-		WWP.GetComponent<SpriteRenderer>().color = tempColor;
 	}
 }
